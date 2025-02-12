106d1206be5877b798ecd9160cce542260b51880
Jegan K <118871085+Jegan1412@users.noreply.github.com>
2025-02-12 11:56:50 +0530

    Update Relational_Logical_Ops_Return_Bool_Values.java

--- Relational_Logical_Ops_Return_Bool_Values.java
+++ Relational_Logical_Ops_Return_Bool_Values.java
@@ -10,12 +10,13 @@ public class Relational_Logical_Ops_Return_Bool_Values
 	    b =  input.nextInt();
 	    c =  input.nextInt();
 	    
-	    
+	    System.out.println("\nShort Circiut Ops:\n");
 	    //Short Circuit Operators
 	    System.out.println(a>b && a>c);
 	    System.out.println(b>a && b>c);
 	    System.out.println(c>b && c>a);
-	    
+
+	    System.out.println("\nNon-Short Circiut Ops:\n");
 	    //Non-Short Circuit Operators
 	    System.out.println(a>b & a>c);
 	    System.out.println(b>a & b>c);
