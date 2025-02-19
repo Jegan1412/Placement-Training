819b3df5c11775ef09d7fd93ab68cd341998bec9
Jegan K <jegankathiravanit@gmail.com>
2025-02-19 22:30:51 +0530

    Refined

--- Matrix_Diagonal_Sum.java
+++ Matrix_Diagonal_Sum.java
@@ -14,7 +14,7 @@ public class Matrix_Diagonal_Sum
 		        arr[i][j]=input.nextInt();
 		    }
 		}
-		
+
 		diagonal_sum(arr, n, 0);
 		
 		display(arr,n,n);
@@ -35,25 +35,27 @@ public class Matrix_Diagonal_Sum
 	
 	static int[][] diagonal_sum(int arr[][], int n, int s){
 	    
-	    int i,j;
+	    int i,j,temp=0;
+		if(arr[s][s] == arr[n-1][n-1]) temp = arr[s][s];
 	    arr[s][s] = 0;
-	    for(i=s+1;i<n;i++){
-	        arr[s][s] += arr[i][s];
-	    }
-	    for(j=s+1;j<n-1;j++){
-	        arr[s][s] += arr[n-1][j];
-	    }
 	    
-	    arr[n-1][n-1]=0;
-	    for(i=n-2;i>=s;i--){
-	        arr[n-1][n-1] += arr[i][n-1];
-	    }
-	    for(j=s+1;j<n-1;j++){
-	        arr[n-1][n-1] += arr[s][j];
-	    }
-	    
-	    if(s!=n) diagonal_sum(arr, n-1, s+1);
-	    
+		for(i=s+1;i<n;i++){
+			arr[s][s] += arr[i][s];
+		}
+		for(j=s+1;j<n-1;j++){
+			arr[s][s] += arr[n-1][j];
+		}
+		arr[n-1][n-1] = 0;
+		for(i=n-2;i>=s;i--){
+			arr[n-1][n-1] += arr[i][n-1];
+		}
+		for(j=n-2;j>s;j--){
+			arr[n-1][n-1] += arr[s][j];
+		}
+	    if(arr[s][s] == arr[n-1][n-1]) arr[s][s] = temp;
+
+		if(s<(n/2)) return diagonal_sum(arr, n-1, s+1);
+
 	    return arr;
 	}
 	
