1e0789eb939828f2fdbca28b464fc0e703cf9a05
Jegan K <118871085+Jegan1412@users.noreply.github.com>
2025-02-20 12:09:22 +0530

    Add files via upload

---
+++ Object_Classes.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Object_Classes
+{
+	public static void main(String[] args) {
+	    
+	    Info n = new Info();
+	    Scanner input = new Scanner(System.in);
+	    ArrayList<String> details_name = new ArrayList<>();
+	    ArrayList<Long> details_reg_no = new ArrayList<>();
+	    int n = input.nextInt();
+	    int i;
+	    for(i=0;i<n;i++){
+	        n.info();
+	        details_name.add(n.name);
+	        details_reg_no.add(n.reg_no);
+	    }
+	    
+	    for(i=0;i<n;i++){
+	        System.out.print(details_name.get(i)+" "+details_reg_no.get(i)+"\n");
+	    }
+	}
+}
+
+class Info{
+    
+    String name;
+    long reg_no;
+    
+    void info(){
+        Scanner input = new Scanner(System.in);
+        name = input.nextLine();
+        reg_no = input.nextLong();
+    }
+    
+    
+}
